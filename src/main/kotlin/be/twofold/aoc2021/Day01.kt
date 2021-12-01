70931551c875e90c5b49afdc842468b695c12727
Jan De Kock <jan@twofold.be>
2021-12-01 17:36:29 +0100

    Solve day 1

--- src/main/kotlin/be/twofold/aoc2021/Day01.kt
+++ src/main/kotlin/be/twofold/aoc2021/Day01.kt
@@ -1,5 +1,21 @@
 package be.twofold.aoc2021
 
+fun part1(input: List<Int>): Int {
+    return input.asSequence()
+        .zipWithNext()
+        .count { it.second > it.first }
+}
+
+fun part2(input: List<Int>): Int {
+    return input.asSequence()
+        .windowed(3) { it.sum() }
+        .zipWithNext()
+        .count { it.second > it.first }
+}
+
 fun main() {
-    println(Util.resource("/day01.txt")[0])
+    val input = Util.readFile("/day01.txt").map { it.toInt() }
+
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
--- src/main/kotlin/be/twofold/aoc2021/Util.kt
+++ src/main/kotlin/be/twofold/aoc2021/Util.kt
@@ -2,7 +2,7 @@ package be.twofold.aoc2021
 
 object Util {
 
-    fun resource(name: String): List<String> {
+    fun readFile(name: String): List<String> {
         val stream = Util.javaClass.getResourceAsStream(name)
             ?: throw IllegalArgumentException("Invalid file: $name")
 
