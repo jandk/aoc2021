0103b7f80f150d08872af10b0560a39aba469086
Jan De Kock <jan@twofold.be>
2021-12-08 20:23:33 +0100

    Solve day 6

---
+++ src/main/kotlin/be/twofold/aoc2021/Day06.kt
@@ -0,0 +1,35 @@
+package be.twofold.aoc2021
+
+object Day06 {
+    fun part1(input: List<Int>): Long {
+        return solve(input, 80)
+    }
+
+    fun part2(input: List<Int>): Long {
+        return solve(input, 256)
+    }
+
+    fun solve(input: List<Int>, days: Int): Long {
+        val groups = LongArray(9)
+        input.forEach { groups[it]++ }
+
+        for (i in 1..days) {
+            val new = groups[0]
+            System.arraycopy(groups, 1, groups, 0, groups.size - 1)
+            groups[6] += new
+            groups[8] = new
+        }
+
+        return groups.sum()
+    }
+}
+
+fun main() {
+    val input = Util.readFile("/day06.txt")
+        .first()
+        .split(',')
+        .map { it.toInt() }
+
+    println("Part 1: ${Day06.part1(input)}")
+    println("Part 2: ${Day06.part2(input)}")
+}
